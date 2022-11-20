d0dfee5b99a0415cd7271cc90b94ac1c2b6741f9
skwarecky <dawid.smalc@gmail.com>
2022-11-20 15:15:21 +0100

    add article show view

--- resources/js/Pages/Article/Show.vue
+++ resources/js/Pages/Article/Show.vue
@@ -1,10 +1,49 @@
 <template>
 
+	<AuthenticatedLayout>
+		<v-row align="center"
+			   justify="center">
+			<v-col cols="6">
+				<v-card class="mt-4">
+					<v-card-title>
+						<v-row>
+							<v-col cols="6">
+								Article
+							</v-col>
+							<v-col class="text-right" cols="6">
+								<LinkButton :link="route('article.index')">Back to list</LinkButton>
+							</v-col>
+						</v-row>
+					</v-card-title>
+					<v-card-text>
+						<div>{{ article.title }}</div>
+						<p> {{ article.content }}</p>
+					</v-card-text>
+				</v-card>
+			</v-col>
+		</v-row>
+
+	</AuthenticatedLayout>
 </template>
 
 <script>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import LinkButton from '@/Components/LinkButton.vue';
+import Table from '@/Components/Table.vue';
+import {useForm} from "@inertiajs/inertia-vue3";
+
 export default {
-	name: "Index.vue"
+	name: "Index.vue",
+	components: {
+		AuthenticatedLayout,
+		LinkButton,
+		Table
+	},
+	props: {
+		article: Object
+	},
+	methods:{
+	}
 }
 </script>
 
