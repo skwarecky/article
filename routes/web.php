86a71a9988e2d9798deb8cfa84774a14b533c772
skwarecky <dawid.smalc@gmail.com>
2022-11-21 09:47:52 +0100

    refractor routes

--- routes/web.php
+++ routes/web.php
@@ -33,10 +33,6 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
 	Route::resource('article', ArticleController::class);
 	Route::resource('asset', AssetController::class)->except(['edit', 'update'])->middleware('editor');
 	Route::get('/asset/{asset}/download', [AssetController::class, 'download'])->name('asset.download');
